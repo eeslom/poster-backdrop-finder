e56eae2315a3c7ca8acea1864c8bc2db83f388a5
Islom Murodov <brilliantpul@gmail.com>
2026-01-12 12:13:55 +0500

    Fix URL format for getPoster function

--- src/composables/getImage.ts
+++ src/composables/getImage.ts
@@ -1,7 +1,7 @@
 import type { Result } from '~/types/Result'
 
 export function getPoster(item: Result): string {
-  return `https://image.tmdb.org/t/p/w370_and_h556_bestv2${item.poster_path}`
+  return `https://image.tmdb.org/t/p/original{item.poster_path}`
 }
 
 export function getBackdrop(item: Result): string {
